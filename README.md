47fc82943f6bd9495c794f6cc060f6603ae4d104
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-24 14:41:43 -0300

    readme

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Made with pygame and win32 apis.
 
 ## Run
 
-Clone or download this repository and open the file ```main.pyw``` or run the command ```python main.pyw```
+Clone or download this repository and open the file ```Desktop Emojis.pyw``` or run the command ```python Desktop Emojis.pyw```
 
 ## Download
 
---
+++ autorun.pyw.bat
@@ -0,0 +1,18 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+for %%A in ("%~dp0") do set "current_path=%%~fA"
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Desktop Emojis.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "!current_path!\Desktop Emojis.pyw" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+
+endlocal
